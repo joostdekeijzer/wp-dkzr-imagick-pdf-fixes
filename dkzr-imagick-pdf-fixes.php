2d07a8783062badaadef06ac63ea2d259f7b48e6
joostdekeijzer <j@dkzr.nl>
2021-07-01 15:50:15 +0200

    Update URI field

--- dkzr-imagick-pdf-fixes.php
+++ dkzr-imagick-pdf-fixes.php
@@ -6,6 +6,7 @@
  * Author: Joost de Keijzer
  * Author URI: https://dkzr.nl
  * Plugin URI: https://dkzr.nl
+ * Update URI: https://dkzr.dev/wp/plugins/dkzr-imagick-pdf-fixes/
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
